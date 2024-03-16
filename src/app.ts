049ab86d90cb4833b1017506620bc562461dc5b9
unknown <smdemon3@gmail.com>
2024-03-16 13:12:01 +0600

    create first user and admin

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ const app: Application = express();
 
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req: Request, res: Response)=> {
     res.send({
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,9 +1,35 @@
-const createAdmin = async () => {
-    return {
-        message: 'Admin created'
-    }
-}
+import { PrismaClient, UserRole } from "@prisma/client";
+import bcrypt from 'bcrypt' 
+
+const prisma = new PrismaClient();
+
+const createAdmin = async (data: any) => {
+
+    const hashedPassword = await bcrypt.hash(data.password, 12)
+
+  const userData = {
+    email: data.admin.email,
+    password: hashedPassword,
+    role: UserRole.ADMIN
+  };
+
+  const result = await prisma.$transaction(async (transactionClient) => {
+
+    const createUserData = await transactionClient.user.create({
+      data: userData,
+    });
+
+    const createAdminData = await transactionClient.admin.create({
+      data: data.admin,
+    });
+
+    return createAdminData
+
+  });
+
+  return result
+};
 
 export const userServices = {
-    createAdmin
-}
+  createAdmin,
+};
